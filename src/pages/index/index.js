94761c54cf2b4d0d95a8f235356e874d8edbe037
yangzhiyang <zhiyang_yang@126.com>
2017-08-16 00:45:16 +0800

    实现首页推荐的数据获取和触底加载

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -5,18 +5,43 @@ import Vue from 'vue'
 import axios from 'axios'
 import url from 'js/api.js'
 
+import {InfiniteScroll} from 'mint-ui'
+Vue.use(InfiniteScroll)
+
 let app = new Vue({
     el: "#app",
     data: {
-        lists: null
+        lists: null,
+        pageNum: 1,
+        pageSize: 6,
+        loading: false,
+        allLoaded: false
     },
     created(){
-        axios.post(url.hotLists, {
-            pageNum: 1,
-            pageSize: 6
-        }).then(res => {
-            console.log(res.data.lists);
-            this.lists = res.data.lists
-        })
+        this.getLists()
+    },
+    methods: {
+        getLists(){
+            if(this.allLoaded) return
+            //防止多次触发
+            this.loading = true
+            axios.post(url.hotLists, {
+                pageNum: this.pageNum,
+                pageSize: this.pageSize
+            }).then(res => {
+                let curLists = res.data.lists
+                //判断所有数据是否加载完毕
+                if(curLists.length < this.pageSize){
+                    this.allLoaded = true
+                }
+                if(this.lists){
+                    this.lists = this.lists.concat(curLists)
+                }else {
+                    this.lists = curLists
+                }
+                this.pageNum++
+                this.loading = false
+            })
+        }
     }
 })
