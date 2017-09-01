78b2a121884da33da00efff1422fb9d6d25d9fea
yangzhiyang <zhiyang_yang@126.com>
2017-09-01 18:54:30 +0800

    完成购物车功能

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -7,7 +7,11 @@ let url = {
   searchList:'/search/list',
   details: '/goods/details',
   deal:'/goods/deal',
-  addCart:'/cart/add'
+  addCart:'/cart/add',
+  cartLists: '/cart/list',
+  cartReduce: '/cart/reduce',
+  cartRemove: '/cart/remove',
+  cartMremove: '/cart/mreomve'
 }
 
 // let host = 'http://rapapi.org/mockjsdata/23334'
